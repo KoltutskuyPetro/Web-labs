b37611df567d97e98ae600e82dcc57a5b53fdb22
Petro Koltutskyi <petrokoltutskuy@gmail.com>
2025-03-31 23:49:19 +0300

    add lab3

---
+++ lab3/Bloggie/Bloggie.Web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+    }
+}
+
---
+++ lab3/Bloggie/Bloggie.Web/Repositories/IImageRespository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Bloggie.Web.Repositories
+{
+    public interface IImageRespository
+    {
+        Task<string> UploadAsync(IFormFile file);
+    }
+}
+
